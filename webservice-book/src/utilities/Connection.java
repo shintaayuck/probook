bb698edf8b6f8d5b69cfb914272040d06e1a3eb4
mnurraihann <raihannaf@gmail.com>
2018-11-26 21:10:26 +0700

    create webservice buy book

--- webservice-book/src/utilities/Connection.java
+++ webservice-book/src/utilities/Connection.java
@@ -1,8 +1,16 @@
 package utilities;
 
+import java.sql.*
+
 public class Connection {
     static String driver = "com.mysql.jdbc.Driver";
     static String database = "jdbc:mysql://localhost:3306/book";
     static String user = "root";
     static String password = "";
+
+    static Connection conn;
+    static Statement stmt;
+    static ResultSet rs;
+
+    public 
 }
