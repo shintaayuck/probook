052dfb4bffddf2a1b24a107fb9d6a28fbfed1fb9
shintaayuck <shinta990405@gmail.com>
2018-11-27 22:23:56 +0700

    search and book detail server done

--- webservice-book/src/services/SOAPPublisherClient.java
+++ webservice-book/src/services/SOAPPublisherClient.java
@@ -12,29 +12,15 @@ public class SOAPPublisherClient {
     public static void main(String[] args) throws MalformedURLException {
         URL wsdlURL = new URL("http://localhost:8000/api/books?wsdl");
     
-        QName qName = new QName("http://services", "BookServiceImplService");
+        QName qName = new QName("http://services/", "BookServiceImplService");
     
         Service service = Service.create(wsdlURL, qName);
         
         BookService bs = service.getPort(BookService.class);
-    
-        Book b1 = new Book();
-        b1.setBookID("1");
-        b1.setTitle("Buku Pertama");
-        String[] a1 = {"Author pertama"};
-        b1.setAuthors(a1);
-        b1.setDescription("Ini buku pertama lho");
-        b1.setBookPrice(10000);
-    
-        Book b2 = new Book();
-        b2.setBookID("2");
-        b2.setTitle("Buku Kedua");
-        String[] a2 = {"Author kedua"};
-        b2.setAuthors(a2);
-        b2.setDescription("Ini buku kedua lho");
-        b2.setBookPrice(20000);
         
-        System.out.println(bs.getBook("1"));
+        
+        bs.searchBook("Shinta");
+        System.out.println("WEY");
     }
 }
 
