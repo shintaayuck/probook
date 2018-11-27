3825fce8f69827fc8f990944617a6135bf7ae97c
mnurraihann <raihannaf@gmail.com>
2018-11-27 16:43:59 +0700

    fix buy book service

--- webservice-book/src/services/SOAPPublisher.java
+++ webservice-book/src/services/SOAPPublisher.java
@@ -5,5 +5,6 @@ import javax.xml.ws.Endpoint;
 public class SOAPPublisher {
     public static void main(String[] args) {
         Endpoint.publish("http://localhost:8000/api/books", new BookServiceImpl());
+        Endpoint.publish("http://localhost:8000/api/buy-books", new BuyBookServiceImpl());
     }
 }
--- webservice-book/src/utilities/Transfer.java
+++ webservice-book/src/utilities/Transfer.java
@@ -1,10 +1,13 @@
 package utilities;
+import org.apache.http.entity.StringEntity;
 import org.json.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.commons.logging.LogFactory;
 
 public class Transfer {
     private String card_no_sender;
@@ -19,19 +22,20 @@ public class Transfer {
 
     public JSONObject transferToBuy() {
         try {
-            String urlpost = "localhost:3000/api/transfer";
+
+            StringEntity pars = new StringEntity(
+                    "{\"card_no_sender\":\"" + this.card_no_sender +
+                    "\",\"card_no_receiver\":\"" + this.card_no_receiver +
+                    "\",\"amount\":" + this.amount + "}");
+
+            String urlpost = "http://localhost:3000/api/transfer";
             HttpClient httpclient = HttpClientBuilder.create().build();
-            HttpPost httppost = new HttpPost(postUrl);
+            HttpPost httppost = new HttpPost(urlpost);
 
-            JSONObject jsonpost = new JSONObject();
-            jsonpost.put("card_no_sender",this.card_no_sender);
-            jsonpost.put("card_no_receiver",this.card.no_receiver);
-            jsonpost.put("amount",this.amount);
-
-            httppost.setEntity((HttpEntity) jsonpost);
+            httppost.setEntity(pars);
             HttpResponse response = httpclient.execute(httppost);
-            JSONObject JSONresult = new JSONObject(response);
-            return JSONresult;
+            JSONObject JSONresponse = new JSONObject(response);
+            return JSONresponse;
         } catch (Exception err) {
             System.out.println(err);
         }
